ed17aa294c76f96db9d72b223a8171d350b83f19
babochkin-prod <68076247+babochkin-prod@users.noreply.github.com>
2022-10-10 11:16:51 +0300

    first-commit

---
+++ src/main/java/com/firstserver/firstserver/controllers/MainController.java
@@ -0,0 +1,16 @@
+package com.firstserver.firstserver.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("title", "first-server");
+        return "index";
+    }
+
+}
